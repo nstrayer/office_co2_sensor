e38cdd8809b5272aeb1d197bd2a7e5d6c2041678
Nick Strayer <nick.strayer@gmail.com>
2021-11-28 09:19:13 -0500

    Updated the app to use database and dbplyr for more logical pulling

--- data_viewer/app.R
+++ data_viewer/app.R
@@ -1,21 +1,20 @@
 library(shiny)
+library(dplyr)
 library(DBI)
-
-con <- dbConnect(RSQLite::SQLite(), "../database/air_quality.db")
-
 source("plot_data.R")
 
+con <- dbConnect(RSQLite::SQLite(), "../database/air_quality.db")
+airquality_db <- tbl(con, "air_quality")
+
 ui <- fluidPage(
-  
-  # Application title
   titlePanel("Air Quality Data from W Madison"),
-  
   sidebarLayout(
     sidebarPanel(
-      actionButton("refresh", "Pull data again")
+      actionButton("refresh", "Pull data again"),
+      sliderInput("nHours", "Number of hours to pull", min = 1, max = 24, value = 12, step = 1)
     ),
     mainPanel(
-      plotOutput("historyPlot"),
+      plotOutput("historyPlot", height = "500px"),
       DT::dataTableOutput("allData")
     )
   )
@@ -24,17 +23,19 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   air_data <- reactive({
-    nobs <- 4000
     input$refresh
+    
+    start_time <- as.integer(Sys.time() - as.difftime(input$nHours, unit="hours"))
     print("Got data from server/database")
-    dbGetQuery(con, paste("SELECT * FROM air_quality ORDER BY time DESC LIMIT", nobs))%>% 
-      as_tibble() %>% 
+    air_data <- airquality_db %>% 
+      filter(time > start_time) %>% 
+      collect() %>% 
       mutate(
         time = as.POSIXct(time, origin="1970-01-01"),
         temp = (temp * 9 / 5) + 32
       ) %>% 
       rename(
-        Time = time, CO2 = co2,Temp = temp, Humidity =  humidity
+        Time = time, CO2 = co2, Temp = temp, Humidity =  humidity
       ) 
   }) 
   
